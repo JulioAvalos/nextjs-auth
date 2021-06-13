03efaaf1c11191c837aa8f8d6023fa75e98bd7ff
JulioAvalos <julioavalos2@msn.com>
2021-06-12 20:21:09 -0600

    sending signup requests from the frontend

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,38 +2,39 @@ import { hashPassword } from "../../../lib/auth";
 import { connectToDatabase } from "../../../lib/db";
 
 async function handler(req, res) {
-  const data = req.body;
+  if (req.method !== "POST") {
+    return;
+  }
+    const data = req.body;
 
-  const { email, password } = data;
+    const { email, password } = data;
 
-  if (
-    !email ||
-    !email.includes("@") ||
-    !password ||
-    password.trim().length < 7
-  ) {
-    res
-      .status(422)
-      .json({
+    if (
+      !email ||
+      !email.includes("@") ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      res.status(422).json({
         message:
           "Invalid input - password should also be at least 7 characteres long.",
       });
-    return;
-  }
+      return;
+    }
 
-  const client = await connectToDatabase();
+    const client = await connectToDatabase();
 
-  const db = client.db();
+    const db = client.db();
 
-  const hashedPassword = hashPassword(password);
+    const hashedPassword = hashPassword(password);
 
-  const result = await db.collection("users").insertOne({
+    const result = await db.collection("users").insertOne({
       email: email,
-      password: hashedPassword
-  });
+      password: hashedPassword,
+    });
 
-  res.status(201).json({message: 'Created user!'});
-
+    res.status(201).json({ message: "Created user!" });
+  
 }
 
 export default handler;
